b92c7daad24131003f4dc681433827ffe6075c57
Pratham <pprathamm123@gmail.com>
2024-09-21 19:16:12 +0530

    up

--- app/src/main/java/com/example/fooddeleivery/Activity/MainActivity.java
+++ app/src/main/java/com/example/fooddeleivery/Activity/MainActivity.java
@@ -11,14 +11,17 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fooddeleivery.Adaptor.CategoryAdaptor;
+import com.example.fooddeleivery.Adaptor.PopularAdaptor;
 import com.example.fooddeleivery.Domain.CategoryDomain;
+import com.example.fooddeleivery.Domain.FoodDomain;
 import com.example.fooddeleivery.R;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-private RecyclerView.Adapter adapter;
-private RecyclerView recyclerViewCategoryList;
+private RecyclerView.Adapter adapter,adapter2;
+private RecyclerView recyclerViewCategoryList,recyclerViewPopularList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,7 @@ private RecyclerView recyclerViewCategoryList;
         setContentView(R.layout.activity_main);
 
         recyclerViewCategory();
+        recyclerViewPopular();
     }
 
     private void recyclerViewCategory() {
@@ -35,9 +39,24 @@ private RecyclerView recyclerViewCategoryList;
         ArrayList<CategoryDomain>category=new ArrayList<>();
         category.add(new CategoryDomain("Pizza","cat_1"));
         category.add(new CategoryDomain("Burger","cat_2"));
-        category.add(new CategoryDomain("Hotdog","cat_3"));
-        category.add(new CategoryDomain("Drink","cat_4"));
-        category.add(new CategoryDomain("Donut","cat_5"));
+        category.add(new CategoryDomain("Sandwich","san"));
+        category.add(new CategoryDomain("Coke","cat_4"));
+        category.add(new CategoryDomain("Frice","download"));
+        adapter=new CategoryAdaptor(category);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+    private void recyclerViewPopular()
+    {
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewPopularList=findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
 
+        ArrayList<FoodDomain> foodList=new ArrayList<>();
+        foodList.add(new FoodDomain("Pepperoni pizza","pop_1","slices pepperoni,mozzerella cheese,fresh oregano,ground black pepper,pizza sauce",199.0));
+        foodList.add(new FoodDomain("Cheese Burger","pop_2","chiken,Gouda Cheese,Special Sauce,Lettuce,Onion",120.0));
+        foodList.add(new FoodDomain("Veg Pizza","pop_3","olive oil,vegetable oil,pitted kalamata,cherry tomatoes,frsh oregeno,basil",160.0));
+
+        adapter2=new PopularAdaptor(foodList);
+        recyclerViewPopularList.setAdapter(adapter2);
     }
 }
